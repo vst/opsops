ae2b0afd223f73511738fea4eeed6c08b3db32ee
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2024-03-11 12:14:32 +0800

    fix(pkg): install shell completions in Nix builds

--- nix/lib/mk-haskell-app.nix
+++ nix/lib/mk-haskell-app.nix
@@ -10,6 +10,7 @@ let
   ## We need these inputs at buildtime:
   extraNativeBuildInputs = [
     pkgs.git
+    pkgs.installShellFiles
     pkgs.makeWrapper
     pkgs.ronn
   ] ++ nativeBuildInputs;
@@ -21,10 +22,23 @@ let
   extraPostFixup = ''
     ## Wrap program:
     wrapProgram $out/bin/${name} --prefix PATH : ${binPath}
+
+    ## Create a temporary directory to store interim artifacts:
+    _tmpdir=$(mktemp -d)
+
+    ## Create completion scripts:
+    $out/bin/${name} --bash-completion-script "$out/bin/${name}" > ''${_tmpdir}/completion.bash
+    $out/bin/${name} --fish-completion-script "$out/bin/${name}" > ''${_tmpdir}/completion.fish
+    $out/bin/${name}  --zsh-completion-script "$out/bin/${name}" > ''${_tmpdir}/completion.zsh
+
+    ## Install completion scripts:
+    installShellCompletion --bash ''${_tmpdir}/completion.bash
+    installShellCompletion --fish ''${_tmpdir}/completion.fish
+    installShellCompletion  --zsh ''${_tmpdir}/completion.zsh
   '';
 in
 pkgs.haskell.lib.justStaticExecutables (
-  drv.overrideAttrs (oldAttrs: rec {
+  drv.overrideAttrs (oldAttrs: {
     nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ extraNativeBuildInputs;
     postFixup = (oldAttrs.postFixup or "") + extraPostFixup;
   })
